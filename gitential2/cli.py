3e1d7d4a284625512ff13f0af5530782037f05d3
László Andrási <mail@laszloandrasi.com>
2020-12-21 13:58:56 +0100

    cli: added command to start http api

--- gitential2/cli.py
+++ gitential2/cli.py
@@ -1,5 +1,5 @@
 import click
-
+import uvicorn
 from gitential2.extraction.repository import extract_incremental
 from gitential2.extraction.output import DataCollector
 
@@ -10,11 +10,18 @@ def cli():
 
 
 @click.command()
-# @click.option("-o", "--output", default="json-stdout")
 @click.argument("clone_url")
 def extract_git_metrics(clone_url):
     output = DataCollector()
     extract_incremental(clone_url=clone_url, output=output)
 
 
+@click.command()
+@click.option("--host", "-h", "host", default="127.0.0.1")
+@click.option("--port", "-p", "port", type=int, default=8080)
+def public_api(host, port):
+    uvicorn.run("gitential2.public_api.main:app", host=host, port=port, log_level="info")
+
+
 cli.add_command(extract_git_metrics)
+cli.add_command(public_api)
