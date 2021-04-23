ae6b0d3bcf2f27265c78441e19d03de68ec9de13
László Andrási <mail@laszloandrasi.com>
2021-04-23 17:44:33 +0200

    legacy_import: fixed importing repositories from account repos

--- gitential2/legacy_import/projects_and_repos.py
+++ gitential2/legacy_import/projects_and_repos.py
@@ -31,7 +31,7 @@ def import_project_and_repos(
         _create_project_repo(g, project_repo, workspace_id=workspace_id)
     for account_repo in legacy_account_repos:
         if not g.backend.repositories.get(workspace_id, id_=account_repo["repo"]["id"]):
-            _import_repo(g, account_repo["repo"], workspace_id)
+            _import_repo_from_account_repo(g, account_repo, workspace_id)
     g.backend.projects.reset_primary_key_id(workspace_id=workspace_id)
     g.backend.repositories.reset_primary_key_id(workspace_id=workspace_id)
     g.backend.project_repositories.reset_primary_key_id(workspace_id=workspace_id)
@@ -101,6 +101,34 @@ def get_clone_protocol(input_str: str) -> str:
 def _import_repo(g: GitentialContext, project_repo: dict, workspace_id: int):
     repo = project_repo["repo"]
     credential_id = project_repo["secret_id"][0] if project_repo.get("secret_id", []) else None
+    try:
+        repo_create = RepositoryInDB(
+            id=repo["id"],
+            clone_url=repo["clone_url"],
+            protocol=get_clone_protocol(repo["clone_url"]),
+            name=get_repo_name(repo["clone_url"]),
+            namespace=get_namespace(repo["clone_url"]),
+            private=repo["private"] if repo["private"] is not None else False,
+            integration_type=get_integration_type(repo["clone_url"])
+            if get_clone_protocol(repo["clone_url"]) != "ssh"
+            else None,
+            integration_name=get_integration_type(repo["clone_url"])
+            if get_clone_protocol(repo["clone_url"]) != "ssh"
+            else None,
+            credential_id=credential_id if get_clone_protocol(repo["clone_url"]) == "ssh" else None,
+            created_at=repo["created_at"],
+            updated_at=repo["updated_at"],
+        )
+        logger.info("Importing repo", workspace_id=workspace_id)
+        g.backend.repositories.insert(workspace_id, repo["id"], repo_create)
+    except ValidationError as e:
+        print(f"Failed to import repo {repo['clone_url']}", e)
+        sys.exit(1)
+
+
+def _import_repo_from_account_repo(g: GitentialContext, account_repo: dict, workspace_id: int):
+    repo = account_repo["repo"]
+    credential_id = account_repo["secret"]["id"] if account_repo.get("secret", {}) else None
     try:
         repo_create = RepositoryInDB(
             id=repo["id"],
