01ef8b7844797d011fd2ae15b6f73a5143c9827e
László Andrási <mail@laszloandrasi.com>
2021-05-31 09:15:39 +0200

    remove unused code

--- gitential2/core/projects.py
+++ gitential2/core/projects.py
@@ -12,7 +12,6 @@ from gitential2.datatypes.repositories import RepositoryCreate  # , RepositorySt
 from .refresh_v2 import refresh_project
 from .context import GitentialContext
 
-# from .statuses import get_repository_status, persist_repository_status, has_repository_status
 
 logger = get_logger(__name__)
 
@@ -50,37 +49,6 @@ def delete_project(g: GitentialContext, workspace_id: int, project_id: int) -> b
     g.backend.project_repositories.update_project_repositories(workspace_id, project_id, [])
     g.backend.projects.delete(workspace_id, project_id)
     return True
-
-
-# def schedule_project_refresh(g: GitentialContext, workspace_id: int, project_id: int, force_rebuild: bool = False):
-#     for repo_id in g.backend.project_repositories.get_repo_ids_for_project(
-#         workspace_id=workspace_id, project_id=project_id
-#     ):
-#         schedule_repository_refresh(g, workspace_id=workspace_id, repository_id=repo_id, force_rebuild=force_rebuild)
-
-
-# def schedule_repository_refresh(
-#     g: GitentialContext, workspace_id: int, repository_id: int, force_rebuild: bool = False
-# ):
-#     def _done_or_stuck(repo_status: RepositoryStatus):
-#         return repo_status.done or repo_status.is_stuck()
-
-#     if (
-#         force_rebuild
-#         or not has_repository_status(g, workspace_id, repository_id)
-#         or _done_or_stuck(get_repository_status(g, workspace_id, repository_id))
-#     ):
-#         repo_status = get_repository_status(g, workspace_id, repository_id)
-#         persist_repository_status(g, workspace_id, repository_id, repo_status.reset())
-
-#         # pylint: disable=import-outside-toplevel,cyclic-import
-#         from .tasks import refresh_repository_task
-
-#         refresh_repository_task.delay(g.settings.dict(), workspace_id, repository_id, force_rebuild)
-#     else:
-#         logger.info(
-#             "Skipping repo refresh task, already scheduled", workspace_id=workspace_id, repository_id=repository_id
-#         )
 
 
 def _update_project_repos(g: GitentialContext, workspace_id: int, project: ProjectInDB, repos=List[RepositoryCreate]):
