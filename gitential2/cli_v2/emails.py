27c2407809337c62375996f4a6adb96f37559d46
László Andrási <mail@laszloandrasi.com>
2021-06-25 10:04:32 +0200

    added g2 emails send for convinience; fixed explore_more_as_professional template

--- gitential2/cli_v2/emails.py
+++ gitential2/cli_v2/emails.py
@@ -3,6 +3,8 @@ from typing import Optional
 import typer
 from structlog import get_logger
 from gitential2.core.tasks import send_scheduled_emails, configure_celery
+from gitential2.core.users import get_user
+from gitential2.core.emails import send_email_to_user
 
 from .common import get_context, OutputFormat, print_results
 
@@ -39,6 +41,17 @@ def cancel_(
 ):
     g = get_context()
     g.backend.email_log.cancel_email(user_id=user_id, template=template_name)
+
+
+@app.command("send")
+def send_(
+    user_id: int,
+    template_name: str,
+):
+    g = get_context()
+    user = get_user(g, user_id=user_id)
+    if user:
+        send_email_to_user(g, user, template_name)
 
 
 @app.command("trigger-sending")
