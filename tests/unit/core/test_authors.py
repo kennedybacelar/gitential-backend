861db6a7a05d87b2106c72d2fe186c0809a77a99
László Andrási <mail@laszloandrasi.com>
2021-06-28 14:05:24 +0200

    deduplication: tests for authors deduplication

---
+++ tests/unit/core/test_authors.py
@@ -0,0 +1,34 @@
+import pytest
+from gitential2.datatypes.authors import AuthorAlias
+from gitential2.core.authors import tokenize_alias, aliases_matching
+
+
+@pytest.mark.parametrize(
+    "test_input,expected",
+    [
+        (AuthorAlias(name="Andrási László", email="laszlo.andrasi@gitential.com"), ["andrasi laszlo"]),
+        (AuthorAlias(login="laszlo.andrasi"), ["andrasi laszlo"]),
+        (AuthorAlias(email="mail@example.com"), []),
+        (
+            AuthorAlias(name="László, ANDRÁSI", email="laco@la.co.hu", login="andrasilaco"),
+            ["andrasi laszlo", "laco", "andrasilaco"],
+        ),
+        (AuthorAlias(name="Александр Панченко"), ["aleksandr panchenko"]),
+        (AuthorAlias(email="loverboy1984@gmail.com"), ["loverboy1984"]),
+    ],
+)
+def test_tokenize_alias(test_input, expected):
+    assert tokenize_alias(test_input) == expected
+
+
+@pytest.mark.parametrize(
+    "first,second,expected",
+    [
+        (AuthorAlias(name="László Andrási"), AuthorAlias(name="Andrási László"), True),
+        (AuthorAlias(name="László Andrási"), AuthorAlias(email="laszlo.andrasi@gitential.com"), True),
+        (AuthorAlias(name="First Test User"), AuthorAlias(name="Second Test User"), False),
+        (AuthorAlias(name="Александр Панченко"), AuthorAlias(name="Aleksandr Panchenko"), True),
+    ],
+)
+def test_aliases_matching(first, second, expected):
+    assert aliases_matching(first, second) == expected
