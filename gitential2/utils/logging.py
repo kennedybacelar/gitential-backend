1eaee29ff11a7b1f92979d544954b5dfee2c6e8c
László Andrási <mail@laszloandrasi.com>
2021-07-20 23:52:59 +0200

    fixed log in testing

--- gitential2/utils/logging.py
+++ gitential2/utils/logging.py
@@ -6,7 +6,7 @@ logger = get_logger(__name__)
 
 
 def log_memory_usage(msg: str, limit=20):
-    if logger.isEnabledFor(logging.DEBUG):
+    if hasattr(logger, "isEnabledFor") and logger.isEnabledFor(logging.DEBUG):
         all_objects = muppy.get_objects()
         sum1 = summary.summarize(all_objects)
         logger.debug(f"MEM USAGE|{msg}", formatted=list(summary.format_(sum1, limit=limit)))
