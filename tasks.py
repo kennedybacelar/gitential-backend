59d4f57f3b35bcaa25b5d44f33ee8329d43ce446
László Andrási <mail@laszloandrasi.com>
2022-01-31 10:52:19 +0100

    updated to python3.10.2

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ from invoke import task
 PACKAGE_DIR = "gitential2"
 TESTS_DIR = "tests"
 DOCKER_REGISTRY = "docker-internal.gitential.io"
-TESTING_IMAGE_TAG = "v3-20220107"
+TESTING_IMAGE_TAG = "v4-20220131"
 
 
 @task
