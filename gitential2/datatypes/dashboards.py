6cec7a272e931308983000e672905f1da37f1529
Benedek Balázs <benbal87@gmail.com>
2022-05-18 18:55:03 +0700

    GTD-1089 - models updated

--- gitential2/datatypes/dashboards.py
+++ gitential2/datatypes/dashboards.py
@@ -1,15 +1,50 @@
-from typing import Optional, List, Tuple
+from enum import Enum
+from typing import Optional, List, Dict, Any, Tuple
 
 from pydantic import Field
 
+from gitential2.datatypes.stats import (
+    MetricName,
+    DimensionName,
+    FilterName,
+    QueryType
+)
 from .common import IDModelMixin, CoreModel, DateTimeModelMixin, ExtraFieldMixin
 from .export import ExportableModel
 
 
+class ChartVisualizationTypes(str, Enum):
+    chart_line_chart_bar = "chart-line_chart-bar"
+    chart_bubble = "chart-bubble"
+    chart_pie = "chart-pie"
+    chart_stacked_bar = "chart-stacked_bar"
+    table = "table"
+
+
+class DashboardChartLayout(CoreModel):
+    x: int
+    y: int
+    w: int
+    h: int
+
+
+class DashboardConfig(CoreModel):
+    type: QueryType
+    filters: Dict[FilterName, Any]
+
+
+class DashboardChartDetails(CoreModel):
+    layout: DashboardChartLayout
+    title: str
+    chart_type: ChartVisualizationTypes
+    metrics: List[MetricName]
+    dimensions: List[DimensionName]
+
+
 class DashboardBase(ExtraFieldMixin, CoreModel):
     title: Optional[str]
-    config: str
-    charts: str
+    config: DashboardConfig
+    charts: List[DashboardChartDetails]
 
 
 class DashboardPublic(IDModelMixin, DateTimeModelMixin, DashboardBase):
