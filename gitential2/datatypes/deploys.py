c0bb188a34eb384ff2c45fbd35dafe97b731744b
Kennedy <kennedybacelar@yahoo.com.br>
2022-05-26 07:47:24 +0200

    returning list of deployments in json format

--- gitential2/datatypes/deploys.py
+++ gitential2/datatypes/deploys.py
@@ -5,29 +5,27 @@ from .common import CoreModel, ExtraFieldMixin, StringIdModelMixin
 
 
 class DeployedPullRequest(StringIdModelMixin, ExtraFieldMixin, CoreModel):
-    repo_id: int
-    number: Optional[int]
-    repo_name: Optional[str]
+    repository_name: Optional[str]
     title: Optional[str]
     created_at: datetime
     merged_at: Optional[datetime] = None
 
 
 class DeployedCommit(CoreModel):
-    repo_id: int
-    pr_number: int
     commit_id: str
+    repository_name: str
+    git_ref: str
 
 
 class DeployedIssue(CoreModel):
-    repo_id: int
-    pr_number: int
+    repository_name: str
     issue_id: str
 
 
 class Deploy(StringIdModelMixin, ExtraFieldMixin, CoreModel):
+    repositories: List[str]
+    environments: List[str]
     pull_requests: List[DeployedPullRequest]
     commits: List[DeployedCommit]
     issues: List[DeployedIssue]
-    environment: str
     deployed_at: datetime
