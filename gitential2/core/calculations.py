f326049b4720c6fad2bff842b703aa5d161c5152
Benedek Balázs <benbal87@gmail.com>
2022-07-08 08:20:11 +0700

    GTD-1172 - :art: highlighting multiplication in velocity calculation

--- gitential2/core/calculations.py
+++ gitential2/core/calculations.py
@@ -364,11 +364,11 @@ def _calculate_commit_level(
     calculated_commits = calculated_commits.join(outlier_df)
 
     calculated_commits["velocity_measured"] = (
-        calculated_commits["loc_i_c"] + 0.2 * calculated_commits["loc_d_c"]
+        calculated_commits["loc_i_c"] + (0.2 * calculated_commits["loc_d_c"])
     ) / calculated_commits["hours_measured"]
     calculated_commits = _add_estimate_hours(_median_measured_velocity(calculated_commits))
     calculated_commits["velocity"] = (
-        calculated_commits["loc_i_c"] + 0.2 * calculated_commits["loc_d_c"]
+        calculated_commits["loc_i_c"] + (0.2 * calculated_commits["loc_d_c"])
     ) / calculated_commits["hours"]
     # calculated_commits["is_bugfix"] = calculated_commits.apply(
     #     lambda x: calculate_is_bugfix(labels=[], title=x["message"]), axis=1
