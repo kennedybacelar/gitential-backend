6cb6460e3d3ebbc54c370dc8cdee5ce2aefed6c8
Benedek Balázs <benbal87@gmail.com>
2022-11-28 21:55:05 +0700

    GTD-1472 :construction: WIP

--- gitential2/backends/sql/cleanup.py
+++ gitential2/backends/sql/cleanup.py
@@ -480,23 +480,33 @@ def __log_delete_results(delete_result: DeleteRowsResult, items_title: str):
 
 
 def __apply_delete_settings_list(wid: int, delete_settings: List[DeleteSettings], c_type: CleanupType) -> CleanupType:
-    for ds in delete_settings:
-        logger.info(f"Cleanup of {ds.items_title} started.")
-        delete_result: DeleteRowsResult = __delete_rows(
-            delete_rows_partial_fn=partial(ds.delete_fn, workspace_id=wid),
-            check_no_rows_partial_fn=partial(ds.count_rows_fn, workspace_id=wid),
-            items_key=ds.delete_ids_key,
-            item_ids_to_delete=ds.item_ids_to_delete,
-            table_name=ds.items_title,
-            wid=wid,
-        )
-        __log_delete_results(delete_result=delete_result, items_title=ds.items_title)
+    redis_key = __get_redis_key_for_cleanup_stage(wid=wid, c_type=c_type)
+    for index, ds in enumerate(delete_settings):
+        redis_value: Optional[int] = 1  # TODO: GET THE REDIS VALUE FROM KVSTORE
+        if not redis_value or (redis_value and redis_value <= index):
+            # TODO: SAVE NEW REDIS VALUE WITH THE CURRENT INDEX
+            # kvstore.save(redis_key, index)
+            logger.info(f"Cleanup of {ds.items_title} started.")
+            delete_result: DeleteRowsResult = __delete_rows(
+                delete_rows_partial_fn=partial(ds.delete_fn, workspace_id=wid),
+                check_no_rows_partial_fn=partial(ds.count_rows_fn, workspace_id=wid),
+                items_key=ds.delete_ids_key,
+                item_ids_to_delete=ds.item_ids_to_delete,
+                table_name=ds.items_title,
+                wid=wid,
+            )
+            __log_delete_results(delete_result=delete_result, items_title=ds.items_title)
+    # TODO: DELETE REDIS KEY
 
     return c_type
 
 
 def __get_redis_key_for_cleanup(wid: int, c_type: CleanupType) -> str:
     return f"cleanup_started_for_workspace_{wid}__cleanup_type:{c_type}"
+
+
+def __get_redis_key_for_cleanup_stage(wid: int, c_type: CleanupType) -> str:
+    return f"cleanup_stage_for_workspace_{wid}__cleanup_type:{c_type}"
 
 
 def __prepare_state_for_residual_data_of_cleanup_process(g: GitentialContext, wid: int) -> bool:
