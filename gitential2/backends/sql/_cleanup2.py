4882b1d58c4c0a31334207018fb805cfea2b64ef
Kennedy <kennedybacelar@yahoo.com.br>
2022-12-22 00:57:39 +0100

    implementing delete_records

--- gitential2/backends/sql/_cleanup2.py
+++ gitential2/backends/sql/_cleanup2.py
@@ -2,7 +2,7 @@ from datetime import date, datetime, timedelta
 from typing import Optional, List
 from enum import Enum
 
-from sqlalchemy import select
+from sqlalchemy import select, or_
 
 from gitential2.core import GitentialContext
 from gitential2.datatypes.cli_v2 import CleanupType
@@ -11,6 +11,10 @@ from gitential2.datatypes.cli_v2 import CleanupType
 class Commits(str, Enum):
     calculated_commits = "calculated_commits"
     extracted_commits = "extracted_commits"
+    extracted_patches = "extracted_patches"
+    calculated_patches = "calculated_patches"
+    extracted_patch_rewrites = "extracted_patch_rewrites"
+    extracted_commit_branches = "extracted_commit_branches"
 
 
 def perform_data_cleanup_(
@@ -20,21 +24,37 @@ def perform_data_cleanup_(
     cleanup_type: Optional[CleanupType] = CleanupType.full,
 ):
 
-    date_to = __get_date_to(g.settings.extraction.repo_analysis_limit_in_days)
-    if date_to:
-        __remove_redundant_commit_data(g, date_to)
+    for workspace_id in workspace_ids:
+        date_to = __get_date_to(g.settings.extraction.repo_analysis_limit_in_days)
+        repo_ids_to_delete = __get_repo_ids_to_delete(g, workspace_id)
+        if date_to:
+            __remove_redundant_commit_data(g, workspace_id, date_to, repo_ids_to_delete)
 
 
-def get_commit_ids_table(g: GitentialContext, date_to: datetime):
-
+def get_commit_ids_table(
+    g: GitentialContext,
+    date_to: datetime,
+    repo_ids_to_delete: List[int],
+):
     # Creating common table expression and returning the commit_ids
     table_ = g.backend.calculated_commits
-    return select([table_.table.c.commit_id]).where(table_.table.c.date <= date_to).cte()
+    return (
+        select([table_.table.c.commit_id])
+        .where(or_(table_.table.c.date <= date_to, table_.table.c.repo_id.in_(repo_ids_to_delete)))
+        .cte()
+    )
 
 
-def __remove_redundant_commit_data(g: GitentialContext, date_to: datetime):
-    cte = get_commit_ids_table(g, date_to)  # common table expression
-    print(cte)
+def __remove_redundant_commit_data(
+    g: GitentialContext,
+    workspace_id: int,
+    date_to: datetime,
+    repo_ids_to_delete: List[int],
+):
+    cte = get_commit_ids_table(g, date_to, repo_ids_to_delete)  # common table expression
+    for commit_table in Commits:
+        table_ = g.backend.__getattribute__(commit_table.value)
+        delete_records(workspace_id, table_, cte)
 
 
 def __get_date_to(number_of_days_diff: Optional[int] = None) -> Optional[datetime]:
@@ -44,5 +64,17 @@ def __get_date_to(number_of_days_diff: Optional[int] = None) -> Optional[datetim
         else None
     )
 
-def delete_records(table_):
-    
+
+def __get_repo_ids_to_delete(g: GitentialContext, workspace_id: int) -> List[int]:
+    repo_ids_all: List[int] = [r.id for r in g.backend.repositories.all(workspace_id=workspace_id)]
+    assigned_repos = {r.repo_id for r in g.backend.project_repositories.all(workspace_id)}
+
+    repos_to_be_deleted = [rid for rid in repo_ids_all if rid not in assigned_repos]
+    return repos_to_be_deleted
+
+
+def delete_records(workspace_id, table_, cte):
+    print("deletando")
+    schema_name = f"ws_{workspace_id}"
+    query = table_.table.delete().where(table_.table.c.commit_id == cte.c.commit_id)
+    table_.engine.execution_options(schema_translate_map={None: schema_name}).execute(query)
