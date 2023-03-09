951f082feaac6809f5d3550188202a6c46393c16
Kennedy <kennedybacelar@yahoo.com.br>
2023-03-09 15:19:01 -0300

    fixing return typing annotation of get_user_id_or_raise_exception

--- gitential2/utils/__init__.py
+++ gitential2/utils/__init__.py
@@ -194,7 +194,7 @@ def get_user_id_or_raise_exception(
     is_at_least_one_id_is_needed: bool = True,
     user_id: Optional[int] = None,
     workspace_id: Optional[int] = None,
-) -> int:
+) -> Union[-1, int]:
     def get_error_msg(line: str):
         return (
             f"Error while trying to refresh {cache_type} cache for user! "
