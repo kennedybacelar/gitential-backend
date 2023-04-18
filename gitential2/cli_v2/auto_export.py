1e9881f7e6f3010ca7943f7813d9cf2755f3cdaa
Kennedy <kennedybacelar@yahoo.com.br>
2023-04-18 20:53:08 -0300

    fixing create_auto_export_

--- gitential2/cli_v2/auto_export.py
+++ gitential2/cli_v2/auto_export.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from typing import Optional, List
 from structlog import get_logger
 import typer
@@ -10,12 +11,17 @@ app = typer.Typer()
 
 
 @app.command("create")
-def create_auto_export_(workspace_id: int, emails: List[str], tempo_access_token: Optional[str] = None):
+def create_auto_export_(
+    workspace_id: int,
+    emails: List[str],
+    tempo_access_token: Optional[str] = typer.Option(None, "--tempo-access-token"),
+    date_from: Optional[datetime] = typer.Option(None, "--date-from"),
+):
     """ """
     g = get_context()
     workspace = g.backend.workspaces.get(id_=workspace_id)
     if workspace:
-        create_auto_export(g, workspace_id, emails, tempo_access_token=tempo_access_token)
+        create_auto_export(g, workspace_id, emails, tempo_access_token=tempo_access_token, date_from=date_from)
     else:
         logger.info(f"Workspace {workspace_id} not found")
         raise typer.Exit(code=1)
